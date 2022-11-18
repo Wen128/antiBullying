09702c60c6c6221466c083a834a6939831a378c9
Wen128 <92354995+Wen128@users.noreply.github.com>
2022-11-18 23:57:20 +0800

    Add files via upload

--- website bully/app.js
+++ website bully/app.js
@@ -1,9 +1,13 @@
 const bubbles = document.querySelectorAll('.bubble');
 let bubblesBurst = 0;
 
+var pop = new Audio("pop.mp3");
+
 bubbles.forEach(bubble => {
     bubble.addEventListener('click', () => {
+        pop.play();
         bubble.style.animation = 'burst 0.4s ease forwards';
+        
         bubblesBurst += 1;
         console.log(bubblesBurst)
 
